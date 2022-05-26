751e9b3176fb4b61692dd77500c106d8668b0796
MielkeDaniel <mielkedani@outlook.de>
2022-05-26 02:40:26 +0200

    hero section design

--- react-frontend-template/src/pages/LandingPage/index.tsx
+++ react-frontend-template/src/pages/LandingPage/index.tsx
@@ -1,8 +1,16 @@
 import TopNav from "./TopNav";
+import DFinityLogo from "./DFinityLogo";
 import "./LandingPage.css";
 
 const LandingPage = () => {
-  return <TopNav />;
+  return (
+    <>
+      <div className="hero-wrapper">
+        <TopNav />
+        <DFinityLogo />
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
